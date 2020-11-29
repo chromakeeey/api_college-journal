b202e73e099afdd71bd0ca50bffefdffe65d0392
Andrew <andrjuxa.ac@gmail.com>
2020-11-29 20:11:51 +0200

    Task Program Edu&Themes done

--- mysql/program.commands.js
+++ mysql/program.commands.js
@@ -1,16 +1,40 @@
 //const QueryHelper = require('../helpers/QueryHelper');
 const { connectionPool } = require('./connection')
 
-const getProgram = async (programId) => {
+/*const getProgram = async (programId) => {
     const [rows] = await connectionPool.query('SELECT * FROM program_education WHERE id = ?', programId);
 
     return rows[0];
-}
-//
-const getProgramThemes = async (programId) => {
-    const [rows] = await connectionPool.query('SELECT pe.name, pe.specialty, pe.course, pt.name, pt.type, FROM program_education AS pe, program_themes AS pt WHERE (pe.id = pt.program_education_id) AND (pe.id = ?)', programId);
+}*/
 
-    return rows[0];
+const getProgram = async (programId) => {
+    // Отримання інфи з бази
+    const [progInfo] = await connectionPool.query('SELECT * FROM program_education WHERE id = ?', programId);
+    const [themInfo] = await connectionPool.query('SELECT * FROM program_themes WHERE program_education_id = ?', programId);
+    
+    console.log('Program education:');
+    console.log(progInfo);
+    console.log('Program themes:');
+    console.log(themInfo.length);
+    console.log(themInfo);
+    // Обєкт для збереження результату
+    let progAndThemes = {};
+    // Формування об'єкта
+    progAndThemes.id = progInfo[0].id;
+    progAndThemes.name = progInfo[0].name;
+    progAndThemes.specialty = progInfo[0].specialty;
+    progAndThemes.course = progInfo[0].course;
+    progAndThemes.subject_id = progInfo[0].subject_id;
+    progAndThemes.themes = [];
+    // Добавлення тем
+    for(let i=0; i<themInfo.length; i++) {
+        let tmpThemes = {};
+        tmpThemes.id = themInfo[i].id;
+        tmpThemes.name = themInfo[i].name;
+        tmpThemes.type = themInfo[i].type;
+        progAndThemes.themes[i] = tmpThemes;
+    }
+    return progAndThemes;
 }
 
 const addProgram = (program) => {
@@ -27,6 +51,5 @@ const addProgram = (program) => {
 
 module.exports = {
     getProgram,
-    getProgramThemes,//
     addProgram
 }
--- routes/program.route.js
+++ routes/program.route.js
@@ -20,18 +20,6 @@ router.get('/programs/:id', [
     res.status(200).json(program);
 } )
 
-//
-router.get('/programs-themes/:id', [
-    param('id').toInt()
-], [
-    // middlewares
-], async (req, res) => {
-    const id = req.params.id;
-    const program = await getProgramThemes(id)
-
-    res.status(200).json(program);
-} )
-
 router.post('/programs', [
     body('specialty_id').isInt(),
     body('course').isInt(),
